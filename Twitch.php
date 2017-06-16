a1158ab9e6e55f62d73c119f0936faa157939a19
nramos <contact@nicolas-r.fr>
2017-06-16 14:08:46 +0200

    Suppression dossier en trop & ajout de quelques méthodes

--- Twitch.php
+++ Twitch.php
@@ -1,5 +1,6 @@
 <?php
 use \GuzzleHttp\Client;
+
 Class Twitch{
 
     private $chaine;
@@ -20,7 +21,7 @@ Class Twitch{
         return json_decode($response->getBody()->getContents());
     }
 
-    private function Channel(){
+    public function Channel(){
         return $this->Stream()->stream->channel;
     }
 
@@ -36,6 +37,10 @@ Class Twitch{
         return is_null($Result->stream);
     }
 
+    public function getChannelId(){
+        return $this->Channel()->_id;
+    }
+
     public function getTitle(){
         if($this->IsOn()){
             return $this->Channel()->status;
@@ -43,11 +48,58 @@ Class Twitch{
             return false;
         }
     }
+
+    public function getFollowers(){
+        if($this->IsOn()){
+            return $this->Channel()->followers;
+        } else {
+            return 0;
+        }
+    }
+
+    public function RequestToken($urlToRedirect){
+
+        $url = $this->twitch.'oauth2/authorize
+                ?client_id='.$this->client_id.'
+                &redirect_uri='.$urlToRedirect.'
+                &response_type=token id_token
+                &scope=channel_editor';
+
+        $this->api->get($url);
+
+    }
+
+    public function GetStreamDuration(){
+        if($this->IsOn()){
+            $now = strtotime("now");
+            $created_at = strtotime($this->Stream()->stream->created_at);
+            return $this->diffDate($created_at, $now);
+        } else {
+            return 0;
+        }
+    }
+    private function diffDate($dateStart, $dateEnd){
+        $start  = date('Y-m-d H:i:s',$dateStart);
+        $end    = date('Y-m-d H:i:s',$dateEnd);
+        $d_start    = new DateTime($start);
+        $d_end      = new DateTime($end);
+        $diff = $d_start->diff($d_end);
+        return $diff->format('Depuis %d jour(s) %h heure(s) et %i minute(s)');
+    }
+    public function SetTitle($access_token = NULL, $title){
+        if(!is_null($access_token)){
+
+        }
+        else {
+            return false;
+        }
+    }
+
     public function getViewers(){
         if($this->IsOn()){
             return $this->Stream()->stream->viewers;
         } else {
-            return false;
+            return 0;
         }
     }
 }
--- index.php
+++ index.php
@@ -4,8 +4,9 @@ error_reporting(E_ALL);
 include_once "vendor/autoload.php";
 include_once "Twitch.php";
 
-$twitch = new Twitch("eclypsiatv", "pfake1inxcypryrgbpihlzbctyhmh5");
+$twitch = new Twitch("jvtv", "pfake1inxcypryrgbpihlzbctyhmh5");
+
 
 echo "<pre>";
-print_r($twitch->getViewers());
+print_r($twitch->GetStreamDuration());
 echo "<pre>";
